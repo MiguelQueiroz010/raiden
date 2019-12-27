cbf2db739c12226e9558db0952f0c9deeb68cdb2
MiguelQueiroz010 <59292183+MiguelQueiroz010@users.noreply.github.com>
2019-12-27 15:50:30 -0300

    Calculadora Console

---
+++ Calculadora/Calculadora/CalcMethods.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    class CalcMethods
+    {
+        public void Normal()
+        {
+
+            int x;
+            int y;
+            string z;
+            int resultado;
+
+            void Valores()
+            {
+                Console.WriteLine("Digite os valores para calcular");
+                Console.WriteLine("Valor 1:");
+                x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Valor 2:");
+                y = int.Parse(Console.ReadLine());
+            }
+
+            Valores();
+            Operacao();
+            void Operacao()
+            {
+                Console.WriteLine("Operações disponíveis: Soma(+), Subtração(-), Divisão(/) e Multiplicação(*)" +
+                    "\nDigite o tipo de operação:");
+                z = Console.ReadLine();
+
+                if (z == "+")
+                {
+                    resultado = x + y;
+                    Console.WriteLine($"\nResultado: {resultado}");
+                }
+                else if (z == "-")
+                {
+                    resultado = x - y;
+                    Console.WriteLine($"\nResultado: {resultado}");
+                }
+                else if (z == "*")
+                {
+                    resultado = x * y;
+                    Console.WriteLine($"\nResultado: {resultado}");
+                }
+                else if (z == "/")
+                {
+                    resultado = x / y;
+                    Console.WriteLine($"\nResultado: {resultado}");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Erro!!");
+                    Operacao();
+                }
+            }
+        }
+        public void Hexadecimal()
+        {
+            Console.WriteLine("Digite os valores para calcular:");
+            Console.WriteLine("Valor 1:");
+            //Hexadecimais vem em strings lembre-se
+            string value1 = Console.ReadLine();
+            //mas são calculados como inteiros com conversão para o valor
+            int i = Convert.ToInt32(value1, 16);
+            Console.WriteLine("Valor 2:");
+            string value2 = Console.ReadLine();
+            int i2 = Convert.ToInt32(value2, 16);
+            Operacao();
+
+            void Operacao()
+            {
+                string p;
+                int res = 0;
+                Console.WriteLine("Digite a operação desejada:");
+                Console.WriteLine("Operações disponíveis: Soma(+), Subtração(-), Divisão(/) e Multiplicação(*)");
+                p = Console.ReadLine();
+                switch (p)
+                {
+                    case "+":
+                        res = i + i2;
+                        Result();
+                        break;
+                    case "-":
+                        res = i - i2;
+                        Result();
+                        break;
+                    case "/":
+                        res = i / i2;
+                        Result();
+                        break;
+                    case "*":
+                        res = i * i2;
+                        Result();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Operação inexistente!!! Tente de novo.");
+                        Operacao();
+                        break;
+                }
+
+                void Result()
+                {
+                    //e para ver o resultado em hexadecimal e não em inteiro use isso:
+                    Console.WriteLine($"\nResultado=" + res.ToString("X"));
+                }
+
+
+            }
+
+
+
+        }
+
+        public void Octal()
+        {
+            Console.WriteLine("Digite os valores para calcular:");
+            Console.WriteLine("Valor 1:");
+            //Octais vem de 1 a 7 e de 10 em diante.
+            string value1 = Console.ReadLine();
+            //mas são calculados como inteiros com conversão para o valor de base 8.
+            int i = Convert.ToInt32(value1, 8);
+            Console.WriteLine("Valor 2:");
+            string value2 = Console.ReadLine();
+            int i2 = Convert.ToInt32(value2, 8);
+            Operacao();
+
+            void Operacao()
+            {
+                string p;
+                int res = 0;
+                Console.WriteLine("Digite a operação desejada:");
+                Console.WriteLine("Operações disponíveis: Soma(+), Subtração(-), Divisão(/) e Multiplicação(*)");
+                p = Console.ReadLine();
+                switch (p)
+                {
+                    case "+":
+                        res = i + i2;
+                        Result();
+                        break;
+                    case "-":
+                        res = i - i2;
+                        Result();
+                        break;
+                    case "/":
+                        res = i / i2;
+                        Result();
+                        break;
+                    case "*":
+                        res = i * i2;
+                        Result();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Operação inexistente!!! Tente de novo.");
+                        Operacao();
+                        break;
+                }
+
+                void Result()
+                {
+                    //e para ver o resultado em octal:
+
+                    Console.WriteLine($"\nResultado=" + Convert.ToString(res, 8));
+                }
+
+
+            }
+
+        }
+        public void Bin()
+        {
+            Console.WriteLine("Digite os valores para calcular:");
+            Console.WriteLine("Valor 1:");
+            //Octais vem de 1 a 7 e de 10 em diante.
+            string value1 = Console.ReadLine();
+            //mas são calculados como inteiros com conversão para o valor de base 8.
+            int i = Convert.ToInt32(value1, 2);
+            Console.WriteLine("Valor 2:");
+            string value2 = Console.ReadLine();
+            int i2 = Convert.ToInt32(value2, 2);
+            Operacao();
+
+            void Operacao()
+            {
+                string p;
+                int res = 0;
+                Console.WriteLine("Digite a operação desejada:");
+                Console.WriteLine("Operações disponíveis: Soma(+), Subtração(-), Divisão(/) e Multiplicação(*)");
+                p = Console.ReadLine();
+                switch (p)
+                {
+                    case "+":
+                        res = i + i2;
+                        Result();
+                        break;
+                    case "-":
+                        res = i - i2;
+                        Result();
+                        break;
+                    case "/":
+                        res = i / i2;
+                        Result();
+                        break;
+                    case "*":
+                        res = i * i2;
+                        Result();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Operação inexistente!!! Tente de novo.");
+                        Operacao();
+                        break;
+                }
+
+                void Result()
+                {
+                    //e para ver o resultado em octal:
+
+                    Console.WriteLine($"\nResultado=" + Convert.ToString(res, 2));
+                }
+
+            }
+        }
+    }
+    }
+
+    
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+namespace Calculadora
+{
+    class Cálculos
+    {
+        static void Main(string[] args)
+        {
+            //Corzinha 
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Principal();
+            static void Principal()
+            {
+                string t;
+                //Chamada de classe
+                CalcMethods exec = new CalcMethods();
+                //Escolha de calculadora
+                Console.WriteLine("Formatos: Normal(n), Hexadecimal(h), Octa(o) e Binário(b)" +
+                    "\nDigite o tipo de calculadora que deseja usar:");
+                t = Console.ReadLine();
+
+                switch (t)
+                {
+                    case "n":
+                        Console.Clear();
+                        exec.Normal();
+                        OpcoesSaida();
+                        break;
+                    case "h":
+                        Console.Clear();
+                        exec.Hexadecimal();
+                        OpcoesSaida();
+                        break;
+                    case "o":
+                        Console.Clear();
+                        exec.Octal();
+                        OpcoesSaida();
+                        break;
+                    case "b":
+                        Console.Clear();
+                        exec.Bin();
+                        OpcoesSaida();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Comando incorreto!!");
+                        Principal();
+                        break;
+                }
+                /* IF ELSE SUBSTITUÍDO POR SWITCH CASE
+                if (t == "n")
+                {
+                    Console.Clear();
+                    exec.Normal();
+                }
+                else if (t == "h")
+                {
+                    Console.Clear();
+                    exec.Hexadecimal();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Comando incorreto!!");
+                    Principal();
+                }
+                */
+                //Execução Pós-cálculos
+                
+            }
+
+            
+            void OpcoesSaida()
+            {
+                Console.WriteLine("\nQuer calcular mais?");
+                Console.WriteLine("Digite 1 para continuar na calculadora.\nDigite 2 para sair." +
+                        "\nEscolha uma opção:");
+                string q = Console.ReadLine();
+                switch (q)
+                {
+                    case "1":
+                        Console.Clear();
+                        Principal();
+                        break;
+                    case "2":
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("OPÇÃO INCORRETA!!");
+                        OpcoesSaida();
+                        break;
+                }
+                }
+
+            }
+        }
+    }
+
